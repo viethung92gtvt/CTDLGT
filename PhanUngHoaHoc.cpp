0dcf6227d020c9dbf6da3f599e6412e1126c7ac2
Nguyễn Việt Hưng <viethung92gtvt@gmail.com>
2020-07-13 15:18:01 +0700

    Create PhanUngHoaHoc.cpp

---
+++ PhanUngHoaHoc.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+map <int,int> Ong;
+map <int,int> phanung;
+void DFS(int n,map phanung){
+	if(n==1) {
+		Ong[1] = 1;
+		return;
+	}
+	if(Ong[n]!=0) I
+}
+int main(){
+	int m,n,a,b,i;
+	cin>>n;
+	cin>>m;
+	for(i=1;i<=m;i++) {
+		cin>>a>>b;
+		phanung[a]=b;
+	}
+}
+
+
